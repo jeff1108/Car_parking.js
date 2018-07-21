f65c72ffdee8c8a08892b556a07dc5de43fa9a57
jeff1108 <jeffhongkong2007@gmail.com>
2018-07-21 14:58:06 +0100

    user can park a car

--- src/carpark.js
+++ src/carpark.js
@@ -1,22 +1,8 @@
-function Player() {
+function CarPark() {
+  this._carpark = [];
 }
-Player.prototype.play = function(song) {
-  this.currentlyPlayingSong = song;
-  this.isPlaying = true;
-};
 
-Player.prototype.pause = function() {
-  this.isPlaying = false;
-};
-
-Player.prototype.resume = function() {
-  if (this.isPlaying) {
-    throw new Error("song is already playing");
-  }
-
-  this.isPlaying = true;
-};
-
-Player.prototype.makeFavorite = function() {
-  this.currentlyPlayingSong.persistFavoriteStatus(true);
-};
+CarPark.prototype.park = function(car) {
+  this._carpark.push(car)
+  return this._carpark;
+}
